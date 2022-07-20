61ac7a41ff36d7ba81c612e31e833aaa40e35640
infl4me <hellpl4y@gmail.com>
2022-07-20 00:11:31 +0000

    add compute_e syscall (all tests passing now, project 1 complete?)

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,6 +10,7 @@
 #include "pagedir.h"
 #include "devices/shutdown.h"
 #include "devices/input.h"
+#include <float.h>
 
 static void syscall_handler(struct intr_frame*);
 bool is_pointer_valid(uint32_t* sp);
@@ -100,6 +101,11 @@ static void syscall_handler(struct intr_frame* f) {
       check_args(args, 1);
 
       f->eax = args[1] + 1;
+      break;
+    case SYS_COMPUTE_E:
+      check_args(args, 1);
+
+      f->eax = sys_sum_to_e(args[1]);
       break;
     case SYS_EXIT:
       check_args(args, 1);
